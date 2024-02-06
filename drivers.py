15d296bf2fe9117475b33f2bfa24557f7de5761a
Plavak Das <plavakdas@gmail.com>
2024-02-07 02:25:16 +0530

    testing list of drivers

---
+++ drivers.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import requests
+
+url = f'http://ergast.com/api/f1/drivers.json?offset=0&limit=999'
+response = requests.get(url).json()
+
+with open('json data/drivers.json','w') as f:
+    json.dump(response['MRData']['DriverTable']['Drivers'],f,indent=4)
